05ab70b24fa4dcc855369f5d72c01461a2d37101
gunfiighter <50083059+gunfiighter@users.noreply.github.com>
2019-05-01 19:43:24 +0500

    Классы разнесены в отдельные файлы.
    Устранена задержка между передвижениями

---
+++ OnceTwiceThrice/GameMap.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace OnceTwiceThrice
+{
+	public class GameMap
+	{
+		public int Width;
+		public int Height;
+		public GameMap(int width, int height)
+		{
+			Width = width;
+			Height = height;
+		}
+
+		public bool IsInsideMap(int x, int y)
+		{
+			return
+				x >= 0 && x < Width &&
+				y >= 0 && y < Height;
+		}
+
+		public bool IsInsideMap(int x, int y, Keys key)
+		{
+			var dx = 0;
+			var dy = 0;
+			switch (key)
+			{
+				case Keys.Up: dy = -1; break;
+				case Keys.Down: dy = 1; break;
+				case Keys.Left: dx = -1; break;
+				case Keys.Right: dx = 1; break;
+				default: throw new ArgumentException();
+			}
+			var newX = x + dx;
+			var newY = y + dy;
+			return
+				IsInsideMap(newX, newY);
+		}
+	}
+}
---
+++ OnceTwiceThrice/MyForm.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace OnceTwiceThrice
+{
+	public class MyForm : Form
+	{
+		public KeyMap CurrentKeyMap;
+
+		public MyForm()
+		{
+			DoubleBuffered = true;
+			CurrentKeyMap = new KeyMap();
+			var map = new GameMap(5, 5);
+
+
+			var image = new ImageModel(this, map, "images/rocket.png", 0, 0);
+			KeyDown += (sender, args) =>
+			{
+				var keyCode = args.KeyCode;
+				CurrentKeyMap.TurnOn(keyCode);
+				image.MakeMove(keyCode);
+				Invalidate();
+			};
+
+			KeyUp += (sender, args) =>
+			{
+				CurrentKeyMap.TurnOff(args.KeyCode);
+			};
+
+
+			var times = 0;
+
+			Paint += (sender, args) =>
+			{
+				var g = args.Graphics;
+				times++;
+				g.DrawImage(image.Image, new Point((int)((image.X + image.xf) * 50), (int)((image.Y + image.yf) * 50)));
+			};
+
+			var timer = new Timer();
+			timer.Interval = 10;
+			timer.Tick += (sender, args) =>
+			{
+				image.MakeAnimation();
+				Invalidate();
+			};
+			timer.Start();
+
+			Invalidate();
+		}
+	}
+}
